9e27320db553185cde4ee900fef51aa3f0034983
tanzeelsahi <tanzeel_sahi@yahoo.com>
2017-06-29 11:12:48 +0100

    message

---
+++ spellchecker/spellcheck_test.py
@@ -0,0 +1,23 @@
+
+import unittest
+from classspellcheck import spellChecker
+class TestSpellChecker(unittest.TestCase):
+
+    def setUp(self):
+        self.spellChecker = spellChecker()
+        self.spellChecker.load_words('spell.words')
+
+    def test_spell_checker(self):
+        self.assertTrue(self.spellChecker.check_word('zygotic'))
+        self.assertFalse(self.spellChecker.check_words('zygotic mistasdas elementary'))
+        self.assertTrue(self.spellChecker.check_words('our first correct sentence'))
+
+# handle case sensitivity
+        self.assertTrue(self.spellChecker.check_words('Our first correct sentence'))
+ 
+# handle full stop
+        self.assertTrue(self.spellChecker.check_words('Our first correct sentence.'))
+
+if __name__ == '__main__':
+    unittest.main()
+
